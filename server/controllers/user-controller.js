bace927046cb55bbceefad89dfbe23dd4b912d00
anlque <an.cr.0999@gmail.com>
2026-04-27 19:06:44 +0400

    fix mangoose connect, user & token models, api router, start user controller

---
+++ server/controllers/user-controller.js
@@ -0,0 +1,57 @@
+class UserController {
+    async registration(req, res, next){
+        try{
+
+        }
+        catch(e){
+
+        }
+    }
+
+    async login(req, res){
+        try{
+
+        }
+        catch(e){
+
+        }
+    }
+
+    async logout(req, res){
+        try {
+
+        }
+        catch(e){
+
+        }
+    }
+    async refresh(req, res){
+        try {
+
+        }
+        catch(e){
+
+        }
+    }
+
+    async activate(req, res){
+        try {
+
+        }
+        catch(e){
+
+        }
+    }
+
+    async getUsers(req, res){
+        try {
+            res.json(['123','456'])
+        }
+        catch(e){
+
+        }
+    }
+}
+
+
+module.exports = new UserController();
