4f6f7e8902699a82be16ffc6469c8996f5b70bea
anlque <an.cr.0999@gmail.com>
2026-04-30 17:54:08 +0400

    fix cors error

--- server/index.js
+++ server/index.js
@@ -11,7 +11,12 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+    // allow cookies
+    credentials: true,
+    // frontend url
+    origin: process.env.CLIENT_URL,
+}));
 app.use('/api', router);
 app.use(errorMiddleware);
 
